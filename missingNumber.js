3570192f70fc8a44617197557247412858e7c62a
mshapir <mshapir95@gmail.com>
2019-06-28 19:22:52 -0400

    missing number solution done

---
+++ missingNumber.js
@@ -0,0 +1,8 @@
+var missingNumber = function(nums) {
+    let sorted = nums.sort((a,b) => a > b ? 1 : a < b ? -1 : 0)
+    for(var i = 0; i < nums.length + 1; i++){
+        if(sorted[i] !== i){
+            return i
+        }
+    }
+};
